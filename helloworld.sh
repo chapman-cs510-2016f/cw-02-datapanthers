0dc1d8e7f0dc55fbd2bbdf980f33c022ba646be9
Justin Dressel <prof.justin.dressel@gmail.com>
2016-09-17 23:39:57 -0700

    instructor comments

--- helloworld.sh
+++ helloworld.sh
@@ -4,4 +4,8 @@ echo -e "Hello world.\n"
 
 exit 0
 
-
+### INSTRUCTOR COMMENTS
+# Note, echo puts a newline by default already, so your code puts two newlines.
+# Also, you can omit the exit 0 at the end of a Bash script as implicit.
+# Following UNIX philosophy, only do things when they are useful: if the default
+# is to exit 0, then you don't need to call exit 0.
