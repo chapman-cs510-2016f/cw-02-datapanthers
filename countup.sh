fe5a55928cab281c4d7d07c727738e45e13cae62
Justin Dressel <prof.justin.dressel@gmail.com>
2016-09-17 23:46:54 -0700

    instructor comments

--- countup.sh
+++ countup.sh
@@ -13,3 +13,10 @@ else
 	exit 1
 fi
 
+### INSTRUCTOR COMMENTS
+# Note the following simplification inside your first if block:
+#   echo $(seq $1)
+# The function seq already does everything you need, so there is no
+# need to reimplement that functionality. In some sense, you run the
+# same code twice here by using seq, then unpacking it to use printf
+# instead.
